0d3d6f0c4ac0ca5625b5a4a3455359cfdad906f3
root <root@localhost.localdomain>
2018-04-19 13:22:58 +0000

    rewrote args for __init__function

--- message.py
+++ message.py
@@ -2,10 +2,14 @@ from time import gmtime, strftime
 
 class meeting:
 
-	def __init__(self,stime = strftime("%I:%M%p",gmtime()),mname,location,topic,email=[],etime):
+	def __init__(self,mname,location,topic,etime,stime = strftime("%I:%M%p",gmtime()),email=[]):
 		self.stime = stime
 		self.mname = mname
 		self.location = location
 		self.topic = topic
-		self.email = email
+		self.email = [email]
 		self.etima = etime
+
+	def add_email(self, email):
+		self.email.append(email)
+
