bea4a1d6ae8cd1f27c5e8e918d9fdfa16d11515f
root <root@localhost.localdomain>
2018-04-19 13:34:46 +0000

    fixed typo of etime and set default as None

--- message.py
+++ message.py
@@ -2,13 +2,13 @@ from time import gmtime, strftime
 
 class meeting:
 
-	def __init__(self,mname,location,topic,etime,stime = strftime("%I:%M%p",gmtime()),email=[]):
+	def __init__(self,mname,location,topic,etime=None,stime = strftime("%I:%M%p",gmtime()),email=[]):
 		self.stime = stime
 		self.mname = mname
 		self.location = location
 		self.topic = topic
-		self.email = [email]
-		self.etima = etime
+		self.email = email
+		self.etime = etime
 
 	def add_email(self, email):
 		self.email.append(email)
